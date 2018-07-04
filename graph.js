4a66099a93686161ab0978843006510f4e3174c5
javisanutr <6019093+javisanutr@users.noreply.github.com>
2018-07-04 23:36:05 +0200

    object

--- graph.js
+++ graph.js
@@ -5,79 +5,87 @@ const NODE_R = 30;
 const WINDOW_WIDTH = canvas.width;
 const WINDOW_HEIGHT = canvas.height;
 
-var nodes = {};
-var arcs = {};
+var nodes = new NodeCollection();
+var arcs = new ArcsCollection();
 
 ctx.translate(WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
-clear();
+clear_graph();
+
 // Node functions
-function add_node(name="", wallet=0, x=0, y=0){
-    // Check if node exists
-    if(Object.keys(nodes).indexOf(name) == -1){
-        nodes[name] = {"x":x, "y":y, "wallet":wallet, "arcs":[]};
-        var option1 = document.createElement("option");
-        option1.text = name;
-        var option2 = document.createElement("option");
-        option2.text = name;
-        document.getElementById("selec1").add(option1);
-        document.getElementById("selec2").add(option2);
-        console.log("Node \"",name,"\" created");
-    }
-    else{
-        console.log("Error, node \"", name, "\" exists");
-    }
-}
+function Node(name="", wallet=0, x=0, y=0){
+    this.name = name;
+    this.wallet = Number(wallet);
+    this.x = x;
+    this.y = y;
 
-function del_node(name){
-    var index;
-    for(keyPos in nodes[name].arcs){
-        console.log("keypos:",keyPos);
-        var key = nodes[name].arcs[keyPos];
-        console.log("key:",key);
-        nodeB = decomp_key(name, key);
-        console.log("nodeB",nodeB);
-        index = nodes[nodeB].arcs.indexOf(key);
-        console.log("index nodeB:",index);
-        //nodes[name].arcs.splice(keyPos,1);
-        //console.log("del key from A:",nodes[name].arcs);
-        nodes[nodeB].arcs.splice(index,1);
-        console.log("del key from B:",nodes[nodeB].arcs);
-        delete arcs[key];
-    }
-    delete nodes[name];
-    del_opt("selec1", name);
-    del_opt("selec2", name);
-}
-
-function del_nodes(){
-    for(element in nodes){
-        del_node(element);
-    }
-}
-
-function draw_node(name="", x=0, y=0){
-    // Node background
-    ctx.fillStyle = "#00FF00";
-    ctx.beginPath();
-    ctx.arc(x,y,NODE_R,0,2*Math.PI);
-    ctx.fill();
-    ctx.stroke();
-
-    // Node name
-    ctx.fillStyle = "#000000";
-    var xText = x - 10 * name.length/4;
-    var yText = y + 2.5;
-    ctx.fillText(name, xText, yText);
+    this.draw_node = function(x=this.x, y=this.y){
+        this.x = x;
+        this.y = y;
+        // Node background
+        ctx.fillStyle = "#00FF00";
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,NODE_R,0,2*Math.PI);
+        ctx.fill();
+        ctx.stroke();
     
-    // Node wallet
-    var wallet = nodes[name].wallet.toString();
-    xText = x - 10 * wallet.length/4;
-    ctx.fillText(wallet, xText, yText+20);
+        // Node name
+        ctx.fillStyle = "#000000";
+        var xText = this.x - 10 * this.name.length/4;
+        var yText = this.y + 2.5;
+        ctx.fillText(this.name, xText, yText);
+        
+        // Node wallet
+        xText = this.x - 10 * this.wallet.toString().length/4;
+        ctx.fillText(this.wallet.toString(), xText, yText+20);
+    
+        // Node shape
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,NODE_R,0,2*Math.PI);
+        ctx.stroke();
+    }
+}
 
-    // Node shape
-    ctx.beginPath();
-    ctx.arc(x,y,NODE_R,0,2*Math.PI);
-    ctx.stroke();
+function NodeCollection(){
+    this.content = {};
+    this.length = function() {return this.keys().length;};
+    this.keys = function() {return Object.keys(this.content);};
+    this.get = function(name) {return this.content[name]};
+    this.set = function(name,obj) {this.content[name] = obj;};
+
+    this.add = function(node){
+        // Check if node exists
+        if(Object.keys(this.content).indexOf(node.name) == -1){
+            this.content[node.name] = node;
+            var option1 = document.createElement("option");
+            var option2 = document.createElement("option");
+            option1.text = node.name;
+            option2.text = node.name;
+            document.getElementById("selec1").add(option1);
+            document.getElementById("selec2").add(option2);
+            console.log("Node \"",node.name,"\" created");
+        }
+        else{
+            console.log("Error, node \"", node.name, "\" exists");
+        }
+    };
+
+    this.delete = function(name){
+        // var index;
+        for(nameB in this.content){
+            if(name != nameB){
+                arcs.delete(gen_arc_key(name, nameB));
+            }
+        }
+        delete this.content[name];
+        del_opt("selec1", name);
+        del_opt("selec2", name);
+    };
+
+    this.delete_all = function(){
+        for(name in this.content){
+            this.delete(name);
+        }
+    }
 }
 
 function calc_pos(i, n){
@@ -119,73 +127,83 @@ function decomp_key(node, key){
 }
 
 // Arcs functions
-function add_arc(nodeA, nodeB, value){
-    var key = gen_arc_key(nodeA, nodeB);
-    arcs[key] = value;
-    nodes[nodeA].arcs.push(key);
-    nodes[nodeB].arcs.push(key);
-}
+function Arc(nodeA, nodeB, value){
+    this.key = gen_arc_key(nodeA.name, nodeB.name);
+    this.nodeA = nodeA;
+    this.nodeB = nodeB;
+    this.value = Number(value);
 
-function del_arc(nodeA, nodeB){
-    var key = gen_arc_key(nodeA, nodeB);
-    delete arcs[key];
-}
-
-function del_arcs(){
-    for(element in arcs){
-        delete arcs[element];
-    }
-}
-
-function draw_arc(nodeA, nodeB){
-    var posA = nodes[nodeA];
-    var posB = nodes[nodeB];
-    // if any node doesn't exist, abort process
-    if(posA == undefined || posB == undefined){
-        return null;
-    }
-    var key = gen_arc_key(nodeA, nodeB);
-    if(arcs[key] == undefined){return null;}
-    var value = arcs[key].toString();
-    // console.log("key:",key);
-    // console.log("value:",value);
-
-    // Draw line
-    ctx.beginPath();
-    ctx.moveTo(posA.x, posA.y);
-    ctx.lineTo(posB.x, posB.y);
-    ctx.stroke();
-
-    var vec = {};
-    vec.x = posB.x - posA.x;
-    vec.y = posB.y - posA.y;
-    round_vec(vec);
-    // console.log("vec:",vec);
-    vecMod = Math.round(Math.sqrt(Math.pow(vec.x,2) + Math.pow(vec.y,2)));
-    var vecnorm = Math.max(Math.abs(vec.x), Math.abs(vec.y));
+    this.draw_arc = function(){
+        // var posA = nodes[nodeA];
+        // var posB = nodes[nodeB];
+        // if any node doesn't exist, abort process
+        // if(posA == undefined || posB == undefined){
+        //     return null;
+        // }
+        console.log("nodeA:",this.nodeA);
+        console.log("nodeB:",this.nodeB);
+            
+        // Draw line
+        ctx.beginPath();
+        ctx.moveTo(this.nodeA.x, this.nodeA.y);
+        ctx.lineTo(this.nodeB.x, this.nodeB.y);
+        ctx.stroke();
     
-    // Draw triangle
-    ctx.save();
-    ctx.translate(Math.round(posA.x + vec.x/2), Math.round(posA.y + vec.y/2));
-    // Arc value
-    var xText = -10 * value.length/4;
-    var yText = 20;
-    ctx.fillStyle = "#000000";
-    ctx.fillText(value, xText, yText);
+        var vec = {};
+        vec.x = this.nodeB.x - this.nodeA.x;
+        vec.y = this.nodeB.y - this.nodeA.y;
+        round_vec(vec);
+        // vecMod = Math.round(Math.sqrt(Math.pow(vec.x,2) + Math.pow(vec.y,2)));
+        // var vecnorm = Math.max(Math.abs(vec.x), Math.abs(vec.y));
+        
+        // Draw triangle
+        ctx.save();
+        ctx.translate(Math.round(this.nodeA.x + vec.x/2), Math.round(this.nodeA.y + vec.y/2));
+        // Arc value
+        var xText = -10 * this.value.toString().length/4;
+        var yText = 20;
+        ctx.fillStyle = "#000000";
+        ctx.fillText(this.value.toString(), xText, yText);
+    
+        ctx.rotate(Math.atan(vec.y/vec.x));
+        console.log("vec:",vec);
+        console.log("angle:",Math.atan(vec.y/vec.x));
+        //if(vec.x<0 || (vec.x==0 && vec.y<0)){ctx.rotate(Math.PI); }
+        if(vec.x<0){ctx.rotate(Math.PI); }
+        ctx.beginPath();
+        ctx.moveTo(5,0);
+        ctx.lineTo(-5,10);
+        ctx.lineTo(-5,-10);
+        ctx.lineTo(5,0);
+        ctx.stroke();
+        ctx.restore();
+    
+        this.nodeA.draw_node();
+        this.nodeB.draw_node();
+    }
+}
 
-    ctx.rotate(Math.atan(vec.y/vec.x));
-    //if(vec.x<0 || (vec.x==0 && vec.y<0)){ctx.rotate(Math.PI); }
-    if(vec.x*vec.y<0){ctx.rotate(Math.PI); }
-    ctx.beginPath();
-    ctx.moveTo(5,0);
-    ctx.lineTo(-5,10);
-    ctx.lineTo(-5,-10);
-    ctx.lineTo(5,0);
-    ctx.stroke();
-    ctx.restore();
+function ArcsCollection(){
+    this.content = {};
+    this.length = function() {return this.keys().length;};
+    this.keys = function() {return Object.keys(this.content);};
+    this.get = function(name) {return this.content[name]};
+    this.set = function(name,obj) {this.content[name] = obj;};
 
-    draw_node(nodeA,posA.x,posA.y);
-    draw_node(nodeB,posB.x,posB.y);
+    this.add = function(arc){
+        var key = gen_arc_key(arc.nodeA.name, arc.nodeB.name);
+        this.content[key] = arc;
+    }
+
+    this.delete = function(key){
+        delete this.content[key];
+    }
+
+    this.delete_all = function(){
+        for(element in this.content){
+            delete this.content[element];
+        }
+    }
 }
 
 function del_opt(idSelec, name){
@@ -209,35 +227,29 @@ function round_vec(vec){
 
 // Graph functions
 function nodes_graph(){
-    var n = Object.keys(nodes).length;
-    Object.keys(nodes).forEach((element,i) => {
-        pos = calc_pos(i,n);
-        draw_node(element, pos.x, pos.y);
-        nodes[element].x = pos.x;
-        nodes[element].y = pos.y;
-    });
+    var n = nodes.length();
+    var pos = {};
+    for(i in nodes.keys()) {
+        console.log("i:",i,"n:",n);
+        pos = calc_pos(Number(i),n);
+        console.log(pos);
+        nodes.get(nodes.keys()[i]).draw_node(pos.x, pos.y);
+    }
 }
 
 function arcs_graph(){
-    var arc;
-    var list = [];
-    var nodeA;
-    var nodeB;
-    for(arc in arcs){
-        list = arc.split(";");
-        if(list.length!=2){return null;}
-        list.sort();
-        draw_arc(list[0], list[1]);
+    for(i in arcs.keys()){
+        arcs.get(arcs.keys()[i]).draw_arc();
     }
 }
 
 function update_graph(){
-    clear();
+    clear_graph();
     nodes_graph();
     arcs_graph();
 }
 
-function clear(){
+function clear_graph(){
     ctx.fillStyle = "#e6e6e6";
     ctx.save();
     ctx.translate(-WINDOW_WIDTH/2,-WINDOW_HEIGHT/2);
